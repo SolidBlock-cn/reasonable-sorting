70f3ad1f5660b03b0cd35de9ba2ff97f73fd979d
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2021-10-17 16:25:50 +0800

    Config your regex!

--- src/main/java/pers/solid/mod/ConfigScreen.java
+++ src/main/java/pers/solid/mod/ConfigScreen.java
@@ -8,11 +8,15 @@ import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class ConfigScreen implements ModMenuApi {
     public static final Map<Item, Collection<Item>> CUSTOM_SORTING_RULES = new HashMap<>();
@@ -20,6 +24,8 @@ public class ConfigScreen implements ModMenuApi {
     static boolean buttonsInDecorations = false;
     static boolean fenceGatesInDecorations = false;
     static boolean swordsInTools = false;
+    static List<String> transferRules = new ArrayList<>();
+    static List<String> regexTransferRules = new ArrayList<>();
 
     public static Map<Item, Collection<Item>> getCustomSortingRules() {
         return CUSTOM_SORTING_RULES;
@@ -71,38 +77,89 @@ public class ConfigScreen implements ModMenuApi {
     private Screen createScreen(Screen previousScreen) {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(previousScreen)
+                .setSavingRunnable(() -> {
+                    MixinHelper.compileItemGroupTransferRules(MixinHelper.ITEM_GROUP_TRANSFER_RULES);
+                })
                 .setTitle(new TranslatableText("title.reasonable-sorting.config"));
 
         ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("category.reasonable-sorting.custom_sorting_rules"));
+        ConfigCategory transfers = builder.getOrCreateCategory(new TranslatableText("category.reasonable-sorting.custom_transfer_rules"));
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         // 自定义排序规则列表
         general.addEntry(entryBuilder
                 .startStrList(new TranslatableText("options.reasonable-sorting.custom_sorting_rules"), customSortingRulesStrList)
-                .setDefaultValue(List.of("# Example: minecraft:stick minecraft:iron_pickaxe"))
+                .setInsertInFront(false)
+                .setExpanded(true)
                 .setAddButtonTooltip(new TranslatableText("options.reasonable-sorting.custom-sorting-rules.add"))
                 .setSaveConsumer(list -> {
                     customSortingRulesStrList = formatted(list);
                     CUSTOM_SORTING_RULES.clear();
                     CUSTOM_SORTING_RULES.putAll(fromStringList(list));
-                    MixinHelper.compileItemGroupTransferRules(MixinHelper.ITEM_GROUP_TRANSFER_RULES);
                 }).build());
 
-        general.addEntry(entryBuilder
+        transfers.addEntry(entryBuilder
                 .startBooleanToggle(new TranslatableText("options.reasonable-sorting.buttons_in_decorations"), buttonsInDecorations)
                 .setSaveConsumer(b -> buttonsInDecorations = b)
                 .build());
 
-        general.addEntry(entryBuilder
+        transfers.addEntry(entryBuilder
                 .startBooleanToggle(new TranslatableText("options.reasonable-sorting.fence_gates_in_decorations"), fenceGatesInDecorations)
                 .setSaveConsumer(b -> fenceGatesInDecorations = b)
                 .build());
 
-        general.addEntry(entryBuilder
+        transfers.addEntry(entryBuilder
                 .startBooleanToggle(new TranslatableText("options.reasonable-sorting.swords_in_tools"), swordsInTools)
-                .setSaveConsumer(b -> swordsInTools = b)
+                .setSaveConsumer(b -> {
+                    swordsInTools = b;
+                })
+                .build());
+
+        transfers.addEntry(entryBuilder
+                .startStrList(new TranslatableText("options.reasonable-sorting.custom_transfer_rules"),transferRules)
+                .setExpanded(true)
+                .setInsertInFront(false)
+                .setSaveConsumer(list -> {
+                    transferRules = list;
+                    ConfigurableGroupTransfer.CUSTOM_TRANSFER_RULE.clear();
+                    for (String s : list) {
+                        final String[] split = s.split("\\s+");
+                        if(split.length<2) continue;
+                        final Identifier id = Identifier.tryParse(split[0]);
+                        if (!Registry.ITEM.containsId(id)) continue;
+                        final Item item = Registry.ITEM.get(id);
+                        ItemGroup itemGroup = getGroupFromId(split[1]);
+                        ConfigurableGroupTransfer.CUSTOM_TRANSFER_RULE.put(item,itemGroup);
+                    }
+                })
+                .build());
+
+        transfers.addEntry(entryBuilder
+                .startStrList(new TranslatableText("options.reasonable-sorting.custom_regex_transfer_rules"),regexTransferRules)
+                .setExpanded(true)
+                .setInsertInFront(false)
+                .setSaveConsumer(list -> {
+                    regexTransferRules = list;
+                    ConfigurableGroupTransfer.CUSTOM_REGEX_TRANSFER_RULE.clear();
+                    for (String s : list) try {
+                        final String[] split = s.split("\\s+");
+                        if(split.length<2) continue;
+                        final Pattern compile = Pattern.compile(split[0]);
+                        final ItemGroup group = getGroupFromId(split[1]);
+                        ConfigurableGroupTransfer.CUSTOM_REGEX_TRANSFER_RULE.put(Objects.requireNonNull(compile),Objects.requireNonNull(group));
+                    } catch (NullPointerException | PatternSyntaxException ignored) {}
+                })
                 .build());
 
         return builder.build();
+    }
+
+    private static @Nullable ItemGroup getGroupFromId(String id) {
+        for (ItemGroup group : ItemGroup.GROUPS) {
+            if (Objects.equals(group.getName(), id)) {
+                return group;
+            }
+        }
+        return null;
     }
 }
