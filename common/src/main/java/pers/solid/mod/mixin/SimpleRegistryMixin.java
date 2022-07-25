04e8abc1b920160a78a2022ac0b7e22933851ba9
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-07-25 16:06:45 +0800

    Fixed potential dead loop

--- common/src/main/java/pers/solid/mod/mixin/SimpleRegistryMixin.java
+++ common/src/main/java/pers/solid/mod/mixin/SimpleRegistryMixin.java
@@ -15,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import pers.solid.mod.SortingRule;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.stream.Stream;
 
 @Mixin(SimpleRegistry.class)
@@ -36,15 +35,6 @@ public abstract class SimpleRegistryMixin<T> extends MutableRegistry<T> {
     final Stream<T> stream = SortingRule.streamOfRegistry(getKey(), rawIdToEntry);
     if (stream != null) {
       cir.setReturnValue(stream.iterator());
-      cir.cancel();
-    }
-  }
-
-  @Inject(method = "getEntries", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/Collections;unmodifiableMap(Ljava/util/Map;)Ljava/util/Map;", shift = At.Shift.BEFORE), cancellable = true)
-  private void reasonableSortedGetEntries(CallbackInfoReturnable<List<T>> cir) {
-    final Stream<T> stream = SortingRule.streamOfRegistry(getKey(), rawIdToEntry);
-    if (stream != null) {
-      cir.setReturnValue(streamEntries().toList());
       cir.cancel();
     }
   }
